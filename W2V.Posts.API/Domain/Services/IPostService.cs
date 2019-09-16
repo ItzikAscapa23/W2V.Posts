2e0571881e1287415cb5c2ee78df8a2df001972a
ItzikAscapa23 <itzik.a23@gmail.com>
2019-09-16 23:43:59 +0300

    Create  PostService that will handle all controller methods

---
+++ W2V.Posts.API/Domain/Services/IPostService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using W2V.Posts.API.Domain.Models;
+using W2V.Posts.API.Domain.Services.Communication;
+
+namespace W2V.Posts.API.Domain.Services
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<Post>> GetAllPosts();
+        Task<PostResponse> CreatePost(Post post);
+        Task<PostResponse> IncrementUpVotes(long postId);
+        Task<PostResponse> IncrementDownVotes(long postId);
+        Task<PostResponse> DeletePost(long postId);
+    }
+}
---
+++ W2V.Posts.API/Domain/Services/PostService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using W2V.Posts.API.Domain.Models;
+using W2V.Posts.API.Domain.Repositories;
+using W2V.Posts.API.Domain.Services.Communication;
+
+namespace W2V.Posts.API.Domain.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly IPostsRepository _postsRepository;
+
+        public PostService(IPostsRepository postsRepository)
+        {
+            _postsRepository = postsRepository;
+        }
+
+        public async Task<IEnumerable<Post>> GetAllPosts()
+        {
+            return await _postsRepository.GetAllPosts();
+        }
+
+        public async Task<PostResponse> CreatePost(Post post)
+        {
+            try
+            {
+                await _postsRepository.CreatePost(post);
+
+                return new PostResponse(post);
+            }
+            catch (Exception ex)
+            {
+                return new PostResponse($"An error occurred when creating Post: {ex.Message}");
+            }
+        }
+
+        public async Task<PostResponse> IncrementUpVotes(long postId)
+        {
+            try
+            {
+                Post existingPost = await _postsRepository.GetPostById(postId);
+
+                if (existingPost == null)
+                {
+                    return new PostResponse($"Post with id={postId} not found.");
+                }
+
+                await _postsRepository.IncrementUpVotes(postId);
+
+                existingPost.UpVotes++;
+                return new PostResponse(existingPost);
+            }
+            catch (Exception ex)
+            {
+                return new PostResponse($"An error occurred when increment UpVotes: {ex.Message}");
+            }
+        }
+
+        public async Task<PostResponse> IncrementDownVotes(long postId)
+        {
+            try
+            {
+                Post existingPost = await _postsRepository.GetPostById(postId);
+
+                if (existingPost == null)
+                {
+                    return new PostResponse($"Post with id={postId} not found.");
+                }
+
+                await _postsRepository.IncrementDownVotes(postId);
+
+                existingPost.DownVotes++;
+                return new PostResponse(existingPost);
+            }
+            catch (Exception ex)
+            {
+                return new PostResponse($"An error occurred when increment DownVotes: {ex.Message}");
+            }
+        }
+
+        public async Task<PostResponse> DeletePost(long postId)
+        {
+            try
+            {
+                Post existingPost = await _postsRepository.GetPostById(postId);
+
+                if (existingPost == null)
+                {
+                    return new PostResponse($"Post with id={postId} not found.");
+                }
+
+                await _postsRepository.DeletePost(postId);
+
+                return new PostResponse(existingPost);
+            }
+            catch (Exception ex)
+            {
+                return new PostResponse($"An error occurred when Delete Post: {ex.Message}");
+            }
+        }
+    }
+}
