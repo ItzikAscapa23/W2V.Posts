c142d3be913674e4c881fb78083e506dd4c59396
ItzikAscapa23 <itzik.a23@gmail.com>
2019-09-16 23:45:50 +0300

    Create response object

---
+++ W2V.Posts.API/Domain/Services/Communication/BaseResponse.cs
@@ -0,0 +1,15 @@
+﻿
+namespace W2V.Posts.API.Domain.Services.Communication
+{
+    public abstract class BaseResponse
+    {
+        public bool Success { get; protected set; }
+        public string Message { get; protected set; }
+
+        protected BaseResponse(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
---
+++ W2V.Posts.API/Domain/Services/Communication/PostResponse.cs
@@ -0,0 +1,29 @@
+﻿using W2V.Posts.API.Domain.Models;
+
+namespace W2V.Posts.API.Domain.Services.Communication
+{
+    public class PostResponse : BaseResponse
+    {
+        public Post Post { get; private set; }
+        public PostResponse(bool success, string message, Post post) : base(success, message)
+        {
+            Post = post;
+        }
+
+        /// <summary>
+        /// Creates a success response.
+        /// </summary>
+        /// <param name="post">Handle post.</param>
+        /// <returns>Response.</returns>
+        public PostResponse(Post post) : this(true, string.Empty, post)
+        { }
+
+        /// <summary>
+        /// Creates am error response.
+        /// </summary>
+        /// <param name="message">Error message.</param>
+        /// <returns>Response.</returns>
+        public PostResponse(string message) : this(false, message, null)
+        { }
+    }
+}
